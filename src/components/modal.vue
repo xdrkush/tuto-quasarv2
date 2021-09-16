bc3d52fba42951aa75ec7b9850127e662e4c46ab
xdrkush <drkushikush@gmail.com>
2021-09-16 20:32:30 +0200

    init: modal v-for

---
+++ src/components/modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-dialog v-model='modal'>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">hello {{ data.name }} {{ data.lastName }}</div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions align="right">
+          <q-btn flat label="Decline" color="primary" @click="$emit('closeModalTest')" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  props: ['modalTest', 'data'],
+  setup (props) {
+    console.log('log', props)
+    const modal = ref(props.modalTest)
+    return {
+      modal
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page class="flex flex-center">
+
+    <div v-for="(user, users) in array" :key='users'>
+      <!-- btn -->
+      <q-btn :label="user.lastName" color="primary" @click="showModal(user)" />
+    </div>
+
+    <!-- Modal -->
+    <ModalTest v-if='modalTest' :modalTest="modalTest" :data='user' @closeModalTest='closeModal()' />
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import ModalTest from 'components/modal.vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+
+  components: {
+    ModalTest
+  },
+
+  methods: {
+    showModal (user) {
+      console.log('Modal open 1', this.modalTest, user)
+      this.user = user
+      this.modalTest = true
+    },
+    closeModal () {
+      console.log('Modal close 1', this.modalTest)
+      this.modalTest = false
+      this.user = null
+    }
+  },
+
+  setup () {
+    const array = [{ name: 'Dr', lastName: 'Kush' }, { name: 'Dr', lastName: 'Hsuk' }]
+    const user = ref({})
+    const modalTest = ref(false)
+    return { modalTest, array, user }
+  }
+})
+</script>
